2925fa57b6ab293dd9458c5a03d8313fdcd6f23e
Hoyoun Song <58301450+410-dev@users.noreply.github.com>
2024-08-27 16:39:46 +0900

    Implemented UI

--- System/Services/UI/main.py
+++ System/Services/UI/main.py
@@ -1,1 +1,32 @@
+import asyncio
+
+import System.stdio as stdio
+
+from System.Library.CoreInfrastructures.execspaces import KernelSpace
+
+doTerminate = False
+
 async def mainAsync(args: list, process):
+    # Switch TTY to 2
+    process.assignedTTY = 2
+    KernelSpace.syscall("drv.io.tty", "switchTTY", 2)
+
+    # Wait for 3 seconds
+    await asyncio.sleep(3)
+
+    stdio.println("KyneOS User Interface")
+
+    while not doTerminate:
+        stdio.printf("Login: ", end="")
+        username = stdio.scanf()
+        stdio.printf("Password: ", end="")
+        password = stdio.scanf()
+
+        success, message, user = KernelSpace.syscall("ext.kyne.authman", "validateUser", username, password, "Local", "localhost", "")
+
+        if not success:
+            stdio.println(message)
+            continue
+        else:
+            stdio.println(message)
+            break
