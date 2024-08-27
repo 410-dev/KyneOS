b1ec9a864ba667916c908607584d1f04f61187c7
Hoyoun Song <58301450+410-dev@users.noreply.github.com>
2024-08-28 04:35:31 +0900

    Changed UI to LogonUI

---
+++ System/Services/LogonUI/main.py
@@ -0,0 +1,42 @@
+import asyncio
+
+import System.stdio as stdio
+from System.Library.CoreInfrastructures.Objects.DSObject import DSObject
+from System.Library.CoreInfrastructures.Objects.Process import Process
+
+from System.Library.CoreInfrastructures.Objects.User import User
+from System.Library.CoreInfrastructures.execspaces import KernelSpace, UserSpace
+
+doTerminate = False
+
+async def mainAsync(args: list, process):
+    # Switch TTY to 2
+    process.assignedTTY = 2
+    KernelSpace.syscall("drv.io.tty", "switchTTY", 2, True)
+
+    stdio.println("KyneOS User Interface")
+
+    while (not doTerminate) and process.isRunning:
+        try:
+            stdio.printf("Login: ", end="")
+            username = stdio.scanf()
+            if username == "exit":
+                stdio.println("Killing initsys...")
+                UserSpace.processes.get(1).kill(0)
+                return
+            stdio.printf("Password: ", end="")
+            password = stdio.scanf()
+
+            success, message, user = KernelSpace.syscall("ext.kyne.authman", "validateUser", username, password, "Local", "localhost", "")
+
+            if not success:
+                stdio.println(message)
+                continue
+            else:
+                stdio.println(message)
+                user: DSObject = user
+                user: User = User(user)
+                UserSpace.openBundle(user, False, f"/System/SystemUserInterfaces/{user.ui}", args)
+        except Exception as e:
+            stdio.println(f"Logon error: {e}")
+            continue
