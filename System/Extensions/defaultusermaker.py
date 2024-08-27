ec765447b6338ea6a258779fc3cefeb896cebca4
Hoyoun Song <58301450+410-dev@users.noreply.github.com>
2024-08-27 16:01:23 +0900

    Creates default user on boot

---
+++ System/Extensions/defaultusermaker.py
@@ -0,0 +1,18 @@
+def DECLARATION() -> dict:
+    return {
+        "type": "ext",  # 3 letter type: ext, drv, svc
+        "class": "setup",
+        "id": "defaultusermaker",
+        "name": "KyneOS Time Manager",
+        "version": "1.0.0",
+        "description": "KyneOS Time Manager Extension",
+        "author": "LKS410",
+        "license": "MIT",
+        "distro": "Universal",
+        "priority": 0
+    }
+
+from System.Library.CoreInfrastructures.execspaces import KernelSpace
+import System.stdio as stdio
+KernelSpace.syscall("ext.kyne.authman", "createUser", "kyne", "kyne", "Local", "localhost", "")
+stdio.println("User 'kyne' created.")
