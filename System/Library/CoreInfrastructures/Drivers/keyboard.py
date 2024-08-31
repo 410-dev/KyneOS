cc33aa0d890ea37245322efa7c3063486b713d6b
Hoyoun Song <58301450+410-dev@users.noreply.github.com>
2024-08-31 14:35:43 +0900

    keyboard: Event could be hooked later

--- System/Library/CoreInfrastructures/Drivers/keyboard.py
+++ System/Library/CoreInfrastructures/Drivers/keyboard.py
@@ -1,9 +1,12 @@
 import asyncio
 import threading
+from collections.abc import Callable
+
 from pynput import keyboard
 
 import System.stdio as stdio
 from System.Library.CoreInfrastructures.execspaces import KernelSpace
+from typing import Callable, List, Tuple
 
 def DECLARATION() -> dict:
     return {
@@ -17,37 +20,39 @@ def DECLARATION() -> dict:
         "license": "MIT",
         "distro": "Universal",
         "priority": 1000,
-        "functions": [
-            "println(str,str):None",
-        ]
+        "functions": []
     }
 
 
 listener = None
+_keySwitches: List[Tuple[List[List[keyboard.Key]], Callable]] = [
+    ([[keyboard.Key.shift, keyboard.Key.f1]], lambda: KernelSpace.syscall("drv.io.tty", "switchTTY", 1)),
+    ([[keyboard.Key.shift, keyboard.Key.f2]], lambda: KernelSpace.syscall("drv.io.tty", "switchTTY", 2)),
+    ([[keyboard.Key.shift, keyboard.Key.f3]], lambda: KernelSpace.syscall("drv.io.tty", "switchTTY", 3)),
+    ([[keyboard.Key.shift, keyboard.Key.f4]], lambda: KernelSpace.syscall("drv.io.tty", "switchTTY", 4)),
+    ([[keyboard.Key.shift, keyboard.Key.f5]], lambda: KernelSpace.syscall("drv.io.tty", "switchTTY", 5)),
+    ([[keyboard.Key.shift, keyboard.Key.f6]], lambda: KernelSpace.syscall("drv.io.tty", "switchTTY", 6)),
+    ([[keyboard.Key.shift, keyboard.Key.f7]], lambda: KernelSpace.syscall("drv.io.tty", "switchTTY", 7)),
+    ([[keyboard.Key.shift, keyboard.Key.f8]], lambda: KernelSpace.syscall("drv.io.tty", "switchTTY", 8)),
+    ([[keyboard.Key.shift, keyboard.Key.f9]], lambda: KernelSpace.syscall("drv.io.tty", "switchTTY", 9))
+]
+
+def addKeySwitch(keys: list[list[keyboard.Key]], callback: Callable):
+    _keySwitches.append((keys, callback))
 
 # TODO Convert this to event handler
 async def mainAsync(args: list):
     current = set()
-    ttySwitchs: dict[int, set] = {
-        1: {keyboard.Key.shift, keyboard.Key.f1},
-        2: {keyboard.Key.shift, keyboard.Key.f2},
-        3: {keyboard.Key.shift, keyboard.Key.f3},
-        4: {keyboard.Key.shift, keyboard.Key.f4},
-        5: {keyboard.Key.shift, keyboard.Key.f5},
-        6: {keyboard.Key.shift, keyboard.Key.f6},
-        7: {keyboard.Key.shift, keyboard.Key.f7},
-        8: {keyboard.Key.shift, keyboard.Key.f8},
-        9: {keyboard.Key.shift, keyboard.Key.f9}
-    }
+
     def on_press(key):
-        for tty, switchKeys in ttySwitchs.items():
-            if key in switchKeys:
-                current.add(key)
-                if all(k in current for k in switchKeys):
-                    stdio.println(f"Switching to TTY {tty}")
-                    KernelSpace.syscall("drv.io.tty", "switchTTY", tty)
-        if key == keyboard.Key.esc:
-            asyncio.run(terminateAsync(0))
+        try:
+            current.add(key)
+            for keys, callback in _keySwitches:
+                if any(all(k in current for k in combination) for combination in keys):
+                    callback()
+                    return
+        except Exception as e:
+            stdio.println(f"Error: {e}")
 
     def on_release(key):
         try:
@@ -62,7 +67,8 @@ async def mainAsync(args: list):
 
 async def terminateAsync(code: int):
     global listener
-    listener.stop()
+    if listener is not None:
+        listener.stop()
 
 def run():
     threading.Thread(target=asyncio.run, args=(mainAsync([]),)).start()
