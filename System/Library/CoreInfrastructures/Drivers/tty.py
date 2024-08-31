3e42aff4aa4ec8f61c46c4e891d71c9d017e8597
Hoyoun Song <58301450+410-dev@users.noreply.github.com>
2024-08-31 14:36:02 +0900

    TTY could be forced to be single

--- System/Library/CoreInfrastructures/Drivers/tty.py
+++ System/Library/CoreInfrastructures/Drivers/tty.py
@@ -27,6 +27,8 @@ def switchTTY(tty: int, silent: bool = False):
     global currentDisplayTTY
     currentDisplayTTY = tty
     from System.Library.CoreInfrastructures.execspaces import KernelSpace
+    if "--enforce-singletty" in KernelSpace.bootArgs:
+        return
     if not fs.isFile(f"/tmp/tty{tty}.log"):
         fs.makeDir(f"/tmp/")
         fs.writes(f"/tmp/tty{tty}.log", "")
@@ -63,8 +65,12 @@ def println(content: str, end: str = "\n", tty: int = -1, dontLeaveLog: bool = F
         else:
             tty = currentDisplayTTY
 
+    if tty == -1:
+        tty = 1
+
     if tty == currentDisplayTTY:
         print(content, end=end)
+
 
     if not dontLeaveLog:
         fs.appends(f"/tmp/tty{tty}.log", content + end)
