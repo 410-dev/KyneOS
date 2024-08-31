0d3a68646c56d96cbe09e9c8bc4467e69e3a73e6
Hoyoun Song <58301450+410-dev@users.noreply.github.com>
2024-08-31 16:22:53 +0900

    stdio allows tty specificatiion

--- System/stdio.py
+++ System/stdio.py
@@ -1,10 +1,10 @@
 from System.Library.CoreInfrastructures.execspaces import KernelSpace
 
-def printf(string: str, end: str = ""):
-    KernelSpace.syscall("drv.io.stdout", "println", string, end=end)
+def printf(string: str, end: str = "", tty: int = -1):
+    KernelSpace.syscall("drv.io.stdout", "println", string, end=end, tty=tty)
 
-def println(string: str):
-    KernelSpace.syscall("drv.io.stdout", "println", string)
+def println(string: str, tty: int = -1):
+    KernelSpace.syscall("drv.io.stdout", "println", string, tty=tty)
 
 def scanf() -> str:
     return KernelSpace.syscall("drv.io.stdin", "scan")
