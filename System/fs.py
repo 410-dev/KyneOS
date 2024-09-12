97a7d6f03d2c0d0b925235621223e178c62a24cb
Hoyoun Song <58301450+410-dev@users.noreply.github.com>
2024-09-13 02:35:31 +0900

    Using global filesys variable to call module, added readb and writeb

--- System/fs.py
+++ System/fs.py
@@ -1,37 +1,45 @@
 from System.Library.CoreInfrastructures.execspaces import KernelSpace
 
+FILESYS = "ext.fs.kfs"
+
 def reads(path: str) -> str:
-    return KernelSpace.syscall("ext.fs.kfs", "reads", path)
+    return KernelSpace.syscall(FILESYS, "reads", path)
+
+def readb(path: str) -> bytes:
+    return KernelSpace.syscall(FILESYS, "readb", path)
 
 def writes(path: str, data: str) -> bool:
-    return KernelSpace.syscall("ext.fs.kfs", "writes", path, data)
+    return KernelSpace.syscall(FILESYS, "writes", path, data)
+
+def writeb(path: str, data: bytes) -> bool:
+    return KernelSpace.syscall(FILESYS, "writeb", path, data)
 
 def appends(path: str, data: str) -> bool:
-    return KernelSpace.syscall("ext.fs.kfs", "appends", path, data)
+    return KernelSpace.syscall(FILESYS, "appends", path, data)
 
 def isFile(path: str) -> bool:
-    return KernelSpace.syscall("ext.fs.kfs", "isFile", path)
+    return KernelSpace.syscall(FILESYS, "isFile", path)
 
 def isDir(path: str) -> bool:
-    return KernelSpace.syscall("ext.fs.kfs", "isDir", path)
+    return KernelSpace.syscall(FILESYS, "isDir", path)
 
 def listDir(path: str) -> list:
-    return KernelSpace.syscall("ext.fs.kfs", "listDir", path)
+    return KernelSpace.syscall(FILESYS, "listDir", path)
 
 def makeDir(path: str) -> bool:
-    return KernelSpace.syscall("ext.fs.kfs", "makeDir", path)
+    return KernelSpace.syscall(FILESYS, "makeDir", path)
 
 def remove(path: str) -> bool:
-    return KernelSpace.syscall("ext.fs.kfs", "remove", path)
+    return KernelSpace.syscall(FILESYS, "remove", path)
 
 def copy(src: str, dest: str) -> bool:
-    return KernelSpace.syscall("ext.fs.kfs", "copy", src, dest)
+    return KernelSpace.syscall(FILESYS, "copy", src, dest)
 
 def move(src: str, dest: str) -> bool:
-    return KernelSpace.syscall("ext.fs.kfs", "move", src, dest)
+    return KernelSpace.syscall(FILESYS, "move", src, dest)
 
 def rename(src: str, dest: str) -> bool:
-    return KernelSpace.syscall("ext.fs.kfs", "rename", src, dest)
+    return KernelSpace.syscall(FILESYS, "rename", src, dest)
 
 def exists(path: str) -> bool:
     return isFile(path) or isDir(path)
