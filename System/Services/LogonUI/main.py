3817d82aa72f0c2fd93d64a4623691256cd03783
Hoyoun Song <58301450+410-dev@users.noreply.github.com>
2024-09-13 02:37:47 +0900

    Update main.py

--- System/Services/LogonUI/main.py
+++ System/Services/LogonUI/main.py
@@ -1,4 +1,5 @@
 import asyncio
+import time
 
 import System.stdio as stdio
 from System.Library.CoreInfrastructures.Objects.DSObject import DSObject
@@ -18,32 +19,40 @@ async def mainAsync(args: list, process):
 
     while (not doTerminate) and process.isRunning:
         try:
-            stdio.printf("Login: ", end="")
-            username = stdio.scanf()
-            if username == "exit":
-                stdio.println("Killing initsys...")
-                for pid, p in UserSpace.processes.items():
-                    if p.executable == "/System/Library/initsys.py":
-                        stdio.println(f"Found initsys at PID {pid}")
-                        p.kill(0)
-                        break
-                # UserSpace.processes.get(1).kill(0)
-                return
-            stdio.printf("Password: ", end="")
-            password = stdio.scanf()
+            currentMachine = DSObject("/Local/localhost")
+            autoLogon = KernelSpace.syscall("ext.kyne.authman", "autoLogonEnabled")
+            if not autoLogon:
+                stdio.printf("Login: ", end="")
+                username = stdio.scanf()
+                if username == "exit":
+                    stdio.println("Killing initsys...")
+                    for pid, p in UserSpace.processes.items():
+                        if p.executable == "/System/Library/initsys.py":
+                            stdio.println(f"Found initsys at PID {pid}")
+                            p.kill(0)
+                            break
+                    # UserSpace.processes.get(1).kill(0)
+                    return
+                stdio.printf("Password: ", end="")
+                password = stdio.scanf()
 
-            if username == "" or password == "":
-                continue
+                if username == "" or password == "":
+                    continue
 
-            success, message, user = KernelSpace.syscall("ext.kyne.authman", "validateUser", username, password, "Local", "localhost", "")
+                success, message, user = KernelSpace.syscall("ext.kyne.authman", "validateUser", username, password, "Local", "localhost", "")
+
+            else:
+                success, message, user = KernelSpace.syscall("ext.kyne.authman", "autoLogon", "Local", "localhost", "")
 
             if not success:
                 stdio.println("Logon error: " + message)
+                time.sleep(currentMachine.getPolicyValue("SystemAdministration.Security.InteractiveLogon.TimeoutPerFailedAttempt", 1))
                 continue
             else:
                 user: DSObject = user
                 user: User = User(user)
-                UserSpace.openBundle(user, False, f"/System/SystemUserInterfaces/{user.ui}", args)
+                UserSpace.openBundle(user, False, f"/System/SystemUserInterfaces/{user.ui}", args, user.home)
+
         except Exception as e:
             stdio.println(f"Logon error: {e}")
             continue
