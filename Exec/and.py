26a0ab4b4f0b711022c036cc633bcf08dcacb1f5
Hoyoun Song <58301450+410-dev@users.noreply.github.com>
2024-09-18 23:44:15 +0900

    Create and.py

---
+++ Exec/and.py
@@ -0,0 +1,24 @@
+import System.shexec as shell
+
+def main(args: list, process):
+    args.pop(0)
+    if len(args) < 2:
+        return 1
+    firstCmd = []
+    for i in range(len(args)):
+        if args[i] == "&&":
+            break
+        firstCmd.append(args[i])
+
+    secondCmd = []
+    for i in range(len(args)):
+        if args[i] == "&&":
+            secondCmd = args[i+1:]
+            break
+
+    firstResult = shell.interpretLine(" ".join(firstCmd), process)
+    if firstResult == 0:
+        result = shell.interpretLine(" ".join(secondCmd), process)
+        return result
+    else:
+        return 1
