d218c69363315d318123588684d696376e3adc32
Hoyoun Song <58301450+410-dev@users.noreply.github.com>
2024-09-18 23:58:35 +0900

    Create ls.py

---
+++ Exec/ls.py
@@ -0,0 +1,25 @@
+from System.Library.CoreInfrastructures.Objects.Process import Process
+import System.stdio as stdio
+import System.fs as fs
+
+def main(args: list, process: Process):
+    args.pop(0)
+    directory: str = " ".join(args)
+    fullDir = f"{process.cwd}/{directory}"
+    if directory == "":
+        output = fs.listDir(process.cwd)
+    elif fs.isFile(fullDir):
+        stdio.println(f"{fullDir}: Not a directory")
+        return 1
+    elif fs.isDir(fullDir):
+        if fs.accessible(fullDir, "r", process):
+            output = fs.listDir(fullDir)
+        else:
+            stdio.println(f"{fullDir}: Permission denied")
+            return 1
+    else:
+        stdio.println(f"{fullDir}: No such file or directory")
+        return 1
+
+    for item in output:
+        stdio.println(item)
