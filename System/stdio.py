6587900b00fd2d372d5e65b202cadb1e7a13f6de
Hoyoun Song <58301450+410-dev@users.noreply.github.com>
2024-09-21 02:56:25 +0900

    Update stdio.py

--- System/stdio.py
+++ System/stdio.py
@@ -1,9 +1,9 @@
 from System.Library.execspaces import KernelSpace
 
-def printf(string: str, end: str = "", tty: int = -1):
+def printf(string: any, end: str = "", tty: int = -1):
     KernelSpace.syscall("drv.io.stdout", "println", f"{string}", end=end, tty=tty)
 
-def println(string: str, tty: int = -1):
+def println(string: any, tty: int = -1):
     KernelSpace.syscall("drv.io.stdout", "println", f"{string}", tty=tty)
 
 def scanf() -> str:
