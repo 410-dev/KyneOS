00b5439cdb4cafb9373a2118e8b2cf16aa2e6391
Hoyoun Song <58301450+410-dev@users.noreply.github.com>
2024-10-02 13:34:47 +0900

    Update framework.py

--- System/Library/Frameworks/KyneLangV1/framework.py
+++ System/Library/Frameworks/KyneLangV1/framework.py
@@ -1,11 +1,2 @@
-def frameworkMain(bundleLocation: str, args: dict, process):
-    pass
-
-
-## Launched when executed scriptsh-exec sync
-def main(args: list, process):
-    pass
-
-## Launched when executed scriptsh-exec async
-async def mainAsync(args: list, process):
+def main(args: dict, process):
     pass
