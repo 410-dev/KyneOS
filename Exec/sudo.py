aef14f088f2a523bc6e252b872da90884ec73db7
Hoyoun Song <58301450+410-dev@users.noreply.github.com>
2024-10-02 13:34:59 +0900

    Update sudo.py

--- Exec/sudo.py
+++ Exec/sudo.py
@@ -1,16 +1,17 @@
 from System.Library.Objects.Process import Process
-from System.Library.Objects.DSObject import DSObject
-from System.Library.Objects.User import User
 
 import System.stdio as stdio
+import System.shexec as shell
 
 def main(args, process: Process):
     stdio.printf("Password >> ", end="")
     password = stdio.scanf()
+    args.pop(0)
     if process.ownerUser.escalatePrivilege(process.ownerUser.username, password):
         try:
-            pass
-            # DO SOMETHING
+            exitCode = shell.interpretLine(" ".join(args), process)
+            process.ownerUser.restoreDefaultPrivilege()
+            return exitCode
         except Exception as e:
             stdio.println(f"Error: {e}")
     else:
