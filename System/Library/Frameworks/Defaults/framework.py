a2288132c32b70d6661e1f6ee195a7e871c964e7
Hoyoun Song <58301450+410-dev@users.noreply.github.com>
2024-10-08 00:25:51 +0900

    meh

---
+++ System/Library/Frameworks/Defaults/framework.py
@@ -0,0 +1,6 @@
+import System.stdio as stdio
+
+def main(args: list, process):
+    stdio.println("Defaults Framework Controller")
+    stdio.println(f"EXEC: {args}")
+    return {"status": "success"}
